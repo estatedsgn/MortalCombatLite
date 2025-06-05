2e2b31ad983abb624826ce7b1ea6c02f48a6d247
estatedsgn <nyakushev4@gmail.com>
2025-06-05 18:14:46 +0300

    2

---
+++ MortalCombatLite/src/main/java/com/mycompany/mortalcombatlite/MortalCombatLite.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mortalcombatlite;
+
+import com.mycompany.mortalcombatlite.UI.GUI;
+
+/**
+ *
+ * @author nyaku
+ */
+public class MortalCombatLite {
+
+    public static void main(String[] args) {
+        GUI gui = new GUI();
+        gui.setVisible(true);
+    }
+}
